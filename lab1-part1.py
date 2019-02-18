25c4550c1d2e45e70235f9298f43bb9c9e224dc8
cyang67 <43709340+cyang67@users.noreply.github.com>
2019-02-17 23:12:12 -0500

    Add files via upload

---
+++ lab1-part1.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 15:59:20 2019
+
+@author: jason
+"""
+
+#logstats2
+#map phase
+
+import re
+import sys
+
+#text = open("log2.txt")
+#text2 = text.readlines()
+
+#reduce phase
+
+dict_ip_count = {}
+
+for line in sys.stdin:
+    line = line.strip()
+    hour_ip, num = line.split('\t')
+    try:
+        num = int(num)
+        dict_ip_count[hour_ip] = dict_ip_count.get(hour_ip, 0) + num
+
+    except ValueError:
+        pass
+
+dict_hour_ip_count = {}
+for hour_ip, count in dict_ip_count.items():
+        hour = hour_ip[1:6]
+        ip = hour_ip[7:]
+        count = int(count)
+        #print(hour)
+        #print(ip)
+        #print(count)
+        if hour not in dict_hour_ip_count.keys():
+            dict_hour_ip_count[hour] = [(ip,count)]
+        else:
+            dict_hour_ip_count[hour].append((ip, count))
+
+for hour, ip_count in dict_hour_ip_count.items():
+    print("The hour is", hour)
+    #print("The ipcount is", ipcount)
+    top_3_ip = sorted(list(ip_count), key = lambda x:x[1], reverse=True)[0:3]
+    print("top 3 ip", top_3_ip)
+    for ip, count in top_3_ip:
+        print ('%s\t%s' % ('['+hour+']' +ip, count))
+
+
+
+
+
+
+
+
+
+
+
+
+
